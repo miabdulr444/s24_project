3562df257da7fce2447e71c736d3467b313688c4
Mohammed Issa Abdul-Rahman <miabdulr@andrew.cmu.edu>
2024-04-22 14:54:34 +0000

    adding pyfiles

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
+"""Setup file for projectpack."""
+
 from setuptools import setup
 
-setup(name = 'projectpack',
-      version = '0.0.1',
-      description = 'project package',
-      maintainer = 'Mohammed Issa Abdul-Rahman',
-      license = 'MIT',
-      packages = ['projectpack'],
-      entry_points = {'console_scripts':['get_info=projectpack.main:get_info']},
-      long_description ='''This package takes on an engineering or science topic
-      as an argument and outputs the title,publication year,author names,institutions
-      and countries those institutions are.''')
+setup(
+    name="projectpack",
+    version="0.0.1",
+    description="project package",
+    maintainer="Mohammed Issa Abdul-Rahman",
+    license="MIT",
+    packages=["projectpack"],
+    entry_points={"console_scripts": ["get_info=projectpack.main:get_info"]},
+    long_description="""This is a query tool for the OpenAlex database.""",
+)
--- test_get_info.py
+++ test_get_info.py
@@ -1,7 +1,33 @@
+"""Module containing unit tests for the projectpack module."""
+
 import projectpack
 
+
 def test_hello():
-    assert projectpack.hello('Guys')=='Hi there Guys'
+    """
+    Test the hello function in projectpack.
+
+    Checks if the hello function returns the expected greeting.
+
+    Returns
+    -------
+        None
+
+    """
+    assert projectpack.hello("Guys") == "Hi there Guys"
+
+
 def get_info(kinetics):
-    assert projectpack.get_info('kinetics')==200
-    
+    """
+    Test the get_info function in projectpack.
+
+    Args
+    ----
+    kinetics: Some parameter representing kinetics.
+
+    Returns
+    -------
+        None.
+
+    """
+    assert projectpack.get_info("kinetics") == 200
