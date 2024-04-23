b95e28c1a04ba17234ad0e8e8521d32676dea8a4
Mohammed Issa Abdul-Rahman <miabdulr@andrew.cmu.edu>
2024-04-23 23:59:42 +0000

    adding pyfiles

--- test_get_info.py
+++ test_get_info.py
@@ -1,33 +1,13 @@
-"""Module containing unit tests for the projectpack module."""
+"""A package for retrieving information from the OpenAlex API."""
 
 import projectpack
 
 
 def test_hello():
-    """
-    Test the hello function in projectpack.
-
-    Checks if the hello function returns the expected greeting.
-
-    Returns
-    -------
-        None
-
-    """
+    """Welcome message."""
     assert projectpack.hello("Guys") == "Hi there Guys"
 
 
 def get_info(kinetics):
-    """
-    Test the get_info function in projectpack.
-
-    Args
-    ----
-    kinetics: Some parameter representing kinetics.
-
-    Returns
-    -------
-        None.
-
-    """
+    """Test the get_info function in the projectpack module."""
     assert projectpack.get_info("kinetics") == 200
